5ea9226fc8dc82eb6226bc588c7d239b87080867
mattiasinokuchi <8-b@tuta.io>
2022-02-21 11:30:55 +0100

    Made demo message more clear

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -42,7 +42,7 @@
 	>
 </nav>
 
-<div hidden={!demoMode}>This is a demo. Everyone can see this.</div>
+<div hidden={!demoMode}>THIS IS A DEMO - ANYONE CAN SEE AND EDIT THIS</div>
 
 <main>
 	<slot />
